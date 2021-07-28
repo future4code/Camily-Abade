1e232bc55e6e65945bb1cba8db84e29b6d4a9de1
camilyAbade <laceyj2.laceyj2@gmail.com>
2021-07-28 15:49:17 -0300

    exercícios feitos

---
+++ semana17/aula51/arquivo-da-aula/template-webservices/src/types.ts
@@ -0,0 +1,21 @@
+
+export type addressInfo = {
+   street: string
+   neighborhood: string
+   city: string
+   state: string
+}
+
+export type userFull = {
+   id: string
+   name: string
+   nickname: string
+   email: string
+   zipcode: string
+   number: number
+   address?: string
+   street: string
+   neighborhood: string
+   city: string
+   state: string
+}
